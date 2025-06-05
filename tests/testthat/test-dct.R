dcb8048273082786203288b13c9fc5b6d0c1bd58
JoFrhwld <JoFrhwld@gmail.com>
2025-06-05 11:17:21 -0400

    fix dct coef test

--- tests/testthat/test-dct.R
+++ tests/testthat/test-dct.R
@@ -1,21 +1,27 @@
-test_that("dct coefs", {
+test_that("dct coefs from package are equal to established library", {
 
+  # Only run test if scipy is available
   skip_if(!require(reticulate))
-
   reticulate::py_config()
   skip_if(!reticulate::py_available())
-
   skip_if(!"scipy" %in% reticulate::py_list_packages()$package)
 
+  # import scipy dct implementation
   scipy <- reticulate::import("scipy")
   py_dct <- scipy$fft$dct
 
+  # generate a curve
   x <- seq(0, 1, length = 10)
   y <- 5 + x + (2 * (x^2)) + (-2 * (x^4))
 
+  # get scipy coefficients & strip dim() to match R vector
   py_coefs <- py_dct(y, norm = "forward", orthogonalize = TRUE)
+  dim(py_coefs) <- NULL
+
+  # package dct
   r_coefs <- tidynorm::dct(y)
 
   expect_equal(length(r_coefs), length(py_coefs))
+  expect_equal(r_coefs, py_coefs)
 
 })
